0ce2bdccbbac01d8ce4f85d4bb31f12cb73fd315
DavidKorochik <david1korochik@gmail.com>
2022-03-15 13:32:18 +0200

    Changed the connection strings to the env variables

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,7 @@
 import { createConnection } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = async () => {
   try {
@@ -6,9 +9,9 @@ export const db = async () => {
       type: 'postgres',
       host: 'localhost',
       port: 8080,
-      username: 'postgres',
-      password: 'davdav0908',
-      database: 'sign2communicate',
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     });
 
     console.log('Connected to the database');
