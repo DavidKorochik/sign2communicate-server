b375685e24b8f6ed6b6fb2bc9bcb5f150350e1fd
DavidKorochik <david1korochik@gmail.com>
2022-03-17 19:04:14 +0200

    Added the db function to the listen function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
-import { db } from './db/db';
+import { db } from './db/postgres/db';
 
 dotenv.config();
 
