22717375bc7ce69b5a38ca0ff920f247aa718f95
DavidKorochik <david1korochik@gmail.com>
2022-03-24 23:08:33 +0200

    Added the auth controllers to the routes

--- src/routes/auth/authRoutes.ts
+++ src/routes/auth/authRoutes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import auth from '../../middleware/auth';
+import {
+  logInUser,
+  getLoggedInUser,
+} from '../../controllers/auth/authController';
 
 const router = express.Router();
 
 // Log in to an existing user @/api/auth
-router.post('/');
+router.post('/', logInUser);
 
 // Get the logged in user @/api/auth
-router.get('/', auth);
+router.get('/', auth, getLoggedInUser);
 
 export default router;
