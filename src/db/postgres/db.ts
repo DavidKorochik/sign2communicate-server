6a95d713413693e249dc4bf80620a95ec6f34a1d
DavidKorochik <david1korochik@gmail.com>
2022-04-23 15:18:47 +0300

    Finished the building

--- src/db/postgres/db.ts
+++ src/db/postgres/db.ts
@@ -7,19 +7,17 @@ dotenv.config();
 
 export const db = async () => {
   try {
-    // await createConnection({
-    //   url: process.env.DATABASE_URL,
-    //   type: 'postgres',
-    //   host: process.env.DB_HOST,
-    //   port: Number(process.env.DB_PORT),
-    //   username: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_NAME,
-    //   entities: [User, Signing],
-    //   synchronize: Boolean(process.env.DB_SYNCHRONIZE),
-    // });
-
-    await createConnection();
+    await createConnection({
+      url: process.env.DATABASE_URL,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Signing],
+      synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    });
 
     console.log('Connected to the database');
   } catch (err) {
